9cf754f19ad2bfc934f158cdb26c98a6779a9ded
Tomoya Koyanagi <tomk79@gmail.com>
2016-12-18 12:34:31 +0900

    各fieldの実装を server と client に分離。

--- fields/client/app.fields.href.js
+++ fields/client/app.fields.href.js
@@ -1,25 +1,4 @@
 module.exports = function(broccoli){
-	var php = require('phpjs');
-	var utils79 = require('utils79');
-
-	/**
-	 * データをバインドする
-	 */
-	this.bind = function( fieldData, mode, mod, callback ){
-		var rtn = '';
-		if(typeof(fieldData)===typeof('')){
-			rtn = utils79.toStr(fieldData);
-			rtn = php.htmlspecialchars( rtn ); // ←HTML特殊文字変換
-			// rtn = rtn.replace(new RegExp('\r\n|\r|\n','g'), '<br />'); // ← 属性値などに使うので、改行コードは改行コードのままじゃないとマズイ。
-		}
-		if( mode == 'canvas' && !rtn.length ){
-			rtn = '';
-		}
-		// setTimeout(function(){
-			callback(rtn);
-		// }, 0);
-		return;
-	}
 
 	/**
 	 * エディタUIを生成 (Client Side)
--- fields/client/app.fields.select.js
+++ fields/client/app.fields.select.js
@@ -2,41 +2,6 @@ module.exports = function(broccoli){
 	var utils79 = require('utils79');
 
 	/**
-	 * データをバインドする
-	 */
-	this.bind = function( fieldData, mode, mod, callback ){
-		var rtn = '';
-		rtn = utils79.toStr(fieldData);
-
-		if( !rtn.length && mod.options ){
-			var isHit = false;
-			for( var idx in mod.options ){
-				if( rtn == mod.options[idx].value ){
-					isHit = true;
-					break;
-				}
-			}
-			if( !isHit ){
-				// 選択値が空白で、空白の選択肢がなければ、1件目のオプションを選ぶ。
-				for( var idx in mod.options ){
-					rtn = mod.options[idx].value;
-					break;
-				}
-			}
-		}
-		if( mode == 'canvas' && !rtn.length ){
-			// rtn = '(ダブルクリックして選択してください)';
-				// ↑未選択時のダミー文はなしにした。
-				// 　クラス名の modifier 部分の拡張などに使用する場合に、
-				// 　クラス名とダミー文が合体して存在しないクラス名になってしまうので。
-		}
-		// setTimeout(function(){
-			callback(rtn);
-		// }, 0);
-		return;
-	}
-
-	/**
 	 * エディタUIを生成
 	 */
 	this.mkEditor = function( mod, data, elm, callback ){
