8a0d7dea8d7c14ae6a6b0b2bdbe35ffd3f98264f
Tomoya Koyanagi <tomk79@gmail.com>
2017-01-24 18:01:48 +0900

    (update submodule)

--- client/dist/libs/px2style/dist/scripts.js
+++ client/dist/libs/px2style/dist/scripts.js
@@ -9816,7 +9816,111 @@ return jQuery;
 
 },{}],2:[function(require,module,exports){
 (function(){
-    var $ = require('jquery');
+    var Px2style = function(){};
+    var px2style = window.px2style = new Px2style;
+    var modal = require('./modal/_modal.js')(Px2style);
 })();
+
+},{"./modal/_modal.js":3}],3:[function(require,module,exports){
+/**
+ * modal.js
+ */
+module.exports = function(Px2style){
+	var $ = require('jquery');
+	var $modal,
+		$target;
+
+	/**
+	 * Open modal dialog.
+	 */
+	Px2style.prototype.modal = function(options, callback){
+		var _this = this;
+		callback = callback||function(){};
+
+		this.closeModal(function(){
+			options = options||{};
+			options.title = options.title||'';
+			options.body = options.body||$('<div>');
+			options.buttons = options.buttons||[
+				$('<button class="px2-btn px2-btn--primary">')
+					.text('OK')
+					.on('click', function(e){
+						_this.closeModal();
+					})
+			];
+			options.target = options.target||$('body');
+
+			var tpl = '';
+			tpl += '<div class="px2-modal">';
+			tpl += ' <div class="px2-modal__dialog">';
+			tpl += '  <div class="px2-modal__header">';
+			tpl += '      <div class="px2-modal__title"></div>';
+			tpl += '  </div>';
+			tpl += '  <div class="px2-modal__body"></div>';
+			tpl += '  <div class="px2-modal__footer"></div>';
+			tpl += ' </div>';
+			tpl += '</div>';
+
+			$modal = $(tpl);
+
+			var $title = $modal.find('.px2-modal__title');
+			$title.append( options.title );
+
+			var $body = $modal.find('.px2-modal__body');
+			$body.append( options.body );
+
+			var $footer = $modal.find('.px2-modal__footer');
+			var $footerUl = $('<ul>');
+			for( var i in options.buttons ){
+				var $li = $('<li>').append(options.buttons[i]);
+				$footerUl.append( $li );
+			}
+			$footer.append($footerUl);
+
+			$target = $(options.target);
+			$target.append($modal);
+
+			if( $target.get(0).tagName.toLowerCase() == 'body' ){
+				// body に挿入する場合は、 fixed に。
+				$modal.css({
+					"position": "fixed"
+				});
+			}else{
+				$modal.css({
+					"height": $target.outerHeight()
+				});
+			}
+
+			callback();
+		});
+
+		return;
+	}
+
+	/**
+	 * Close modal dialog.
+	 */
+	Px2style.prototype.closeModal = function(callback){
+		callback = callback||function(){};
+		try {
+			$modal.remove();
+		} catch (e) {}
+		callback();
+		return;
+	}
+
+	/**
+	 * Window Resize Event
+	 */
+	$(window).on('resize', function(){
+		try {
+			if( $target.get(0).tagName.toLowerCase() != 'body' ){
+				$modal.css({
+					"height": $target.outerHeight()
+				});
+			}
+		} catch (e) {}
+	});
+}
 
 },{"jquery":1}]},{},[2])
