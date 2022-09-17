20f4646d042a65fd6fa198be883abe0944a61f33
Tomoya Koyanagi <tomk79@gmail.com>
2022-09-17 22:31:01 +0900

    add comment

--- tests/utils/file2base64.php
+++ tests/utils/file2base64.php
@@ -1,10 +1,14 @@
 <?php
 /****
-README
+
+# README
 これは、コマンドライン引数で受け取ったファイルのパスから、
 Base64文字列を作成して 表示します。
 webpack の url-loader も asset/inline も、base64 を正しく変換できなかったため、
 暫定的に base64 に手動変換して対応するために置いたスクリプトです。
+
+# USAGE
+php ./tests/utils/file2base64.php ./client/src/resources/chevron-down-x2.png;
 ****/
 require_once(__DIR__.'/../../vendor/autoload.php');
 $fs = new tomk79\filesystem();
